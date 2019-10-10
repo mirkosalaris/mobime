e91c87c266fa7533736ab08291e2c0f2758dd4ac
Mirko Salaris <salarismirko@gmail.com>
2019-10-10 18:56:45 +0200

    working homepage

---
+++ app/static/basic.js
@@ -0,0 +1,47 @@
+function react(event) {
+    let value = $("#search-box input")[0].value;
+
+    fillAvanzata(value);
+
+    value = value.toLowerCase(); // to support case insensitive matching
+
+
+    if (value.length > 0) {
+        if ("firenze".indexOf(value) === 0) {
+            showFirenze();
+        } else if ("cinema".indexOf(value) === 0) {
+            showCinema();
+        } else {
+            hideEverything();
+        }
+    } else {
+        hideEverything();
+    }
+}
+
+function fillAvanzata(value) {
+    // put the text of the search after "ricerca avanzata"
+    $(".searchString").html(value)
+}
+
+function showFirenze() {
+    $("#search-result").show();
+    $("#firenze").show();
+     $("#cinema").hide();
+}
+
+function showCinema() {
+    $("#search-result").show();
+    $("#cinema").show();
+     $("#firenze").hide();
+}
+
+function hideEverything() {
+    $("#firenze").hide();
+    $("#cinema").hide();
+    $("#search-result").hide();
+}
+
+
+$("#search-box > form").keyup(react);
+console.log("ciao");
