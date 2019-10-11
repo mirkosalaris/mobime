c7931c6e482d59b60b2f9c427aa02cdc8c680cd9
r0metheus <francesco3.romeo@mail.polimi.it>
2019-10-11 02:04:51 +0200

    new login page

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,15 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html", message="Hello Flask!", contacts=['c1', 'c2', 'c3', 'c4', 'c5'])
+
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/profile.html')
+def profile():
+    return render_template('profile.html')
